93a7367b8b518d766d748ac72859669c4866a105
snake00 <maousnake00@live.jp>
2018-09-25 13:06:54 +0900

    delete ping(test)

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	ping := controllers.NewPingController(r)
-	ping.GetPing()
-	ping.PostPing()
-
 	twitter := controllers.NewTwitterController(r)
 	twitter.GetCrcToken()
 	twitter.PostWebHook()
--- parameters/responses/ping_response.go
+++
@@ -1,6 +0,0 @@
-package responses
-
-type GetPingResponse struct {
-	Message string `json:"message"`
-	Test    string `json:"test"`
-}
